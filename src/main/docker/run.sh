b5a3857f0887ea6e8d24c1377d5886308c59cf55
Carl J. Mosca <carljmosca@gmail.com>
2016-11-12 20:54:00 -0500

    refactored

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,3 +1,3 @@
-docker stop filemanager
-docker rm filemanager
-docker run -d -p 8090:8080 --name filemanager cad/filemanager
+docker stop vaadin8-demo
+docker rm vaadin8-demo
+docker run -d -p 8090:8080 --name vaadin8-demo demo/vaadin8-demo
